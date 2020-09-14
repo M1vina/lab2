8658b137ccaf8e40ebca1f8cb1a48d9a1595eeeb
M1vina <70892253+M1vina@users.noreply.github.com>
2020-09-14 14:21:32 +0300

    Add files via upload

---
+++ Author/Author.java
@@ -0,0 +1,39 @@
+package Author;
+
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author (String name, String email,char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "имя '" + name + '\'' +
+                        "(" + gender + ")" +
+                        ", at '" + email + '\'' +
+                        '}';
+    }
+}
+
---
+++ Author/TestAuthor.java
@@ -0,0 +1,9 @@
+package Author;
+
+public class TestAuthor {
+    public static void main(String[] ae) {
+        Author auth1 = new Author("Вася", "Vasya@gmail.com", 'm');
+        System.out.println(auth1);
+    }
+}
+
